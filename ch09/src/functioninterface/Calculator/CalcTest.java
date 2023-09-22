b37740f80d2dc379ff67a793483160fb9f35c7ea
kukulza1 <wlskrkek12345@naver.com>
2023-09-22 18:14:31 +0900

    09/22

--- ch09/src/functioninterface/Calculator/CalcTest.java
+++ ch09/src/functioninterface/Calculator/CalcTest.java
@@ -9,16 +9,19 @@ public class CalcTest {
 	    int num2 =5;
 		
 		
-		add=(n1, n2)->(num1+num2);
+		add=(n1, n2)->(n1+n2);
+		int v1 =add.Calculate(num1, num2);
 		System.out.println("add="+add.Calculate(num1, num2));
 		
-		sub=(n1,n2)->(num1-num2);
+		sub=(n1,n2)->(n1-n2);
+		int v2=sub.Calculate(num1, num2);
 		System.out.println("sub="+sub.Calculate(num1, num2));
-		
-		mul=(n1,n2)->(num1*num2);
+		mul=(n1,n2)->(n1*n2);
+		int v3 = mul.Calculate(num1, num2);
 		System.out.println("mul="+mul.Calculate(num1, num2));
           
-		div=(n1,n2)->(num1/num2);
+		div=(n1,n2)->(n1/n2);
+		double v4 = div.Calculate(num1, num2);
 		System.out.println("div="+(double)div.Calculate(num1, num2));
 	}
 
